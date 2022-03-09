41186f7eb1fcf8458d6edd4b98b882a06ed3e1d0
ecsimsw <ecsimsw@gmail.com>
2022-03-10 05:09:24 +0900

    feat : load memory

---
+++ src/main/kotlin/computer/architecture/MainApplication.kt
@@ -0,0 +1,10 @@
+package computer.architecture
+
+import computer.architecture.memory.Memory
+
+fun main() {
+    val inputPath = "input/Von Neumann architecture.txt"
+    val memory = Memory.load(inputPath)
+
+
+}
---
+++ src/main/kotlin/computer/architecture/memory/Memory.kt
@@ -0,0 +1,20 @@
+package computer.architecture.memory
+
+import java.io.File
+
+class Memory(
+    private val instructions: Array<String>
+) {
+
+    companion object {
+        fun load(path: String): Memory {
+            val instructions: MutableList<String> = arrayListOf();
+            File(path).forEachLine { instructions.add(it) }
+            return Memory(instructions.toTypedArray())
+        }
+    }
+
+    operator fun get(index : Int) : String {
+        return instructions[index]
+    }
+}
